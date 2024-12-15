daf3dc1b0e71e2cc47ea6318461a50983aa53ed1
Nick Tikhonov <ndtikhonov@gmail.com>
2024-12-15 15:35:07 +0100

    Rescrape

--- src/app/t/[ca]/page.tsx
+++ src/app/t/[ca]/page.tsx
@@ -9,12 +9,13 @@ import { Nav, TradeApp } from "~/app/app";
 import { getOrScrapeByCa } from "~/lib/clanker";
 import { serverFetchCA } from "~/app/server";
 import { track } from "@vercel/analytics/server";
+import { Metadata } from "next";
 
 type Params = Promise<{ca: string}>;
 
 export async function generateMetadata({ params }: {
   params: Params
-}) {
+}): Promise<Metadata>  {
   const { ca } = await params
   await getOrScrapeByCa(ca)
   const data = await serverFetchCA(ca)
@@ -39,16 +40,14 @@ export async function generateMetadata({ params }: {
       title,
       description,
       url: `https://clank.fun/t/${data.contract_address}`,
-      logo: `https://clank.fun/og.png`,
-      type: 'website'
+      type: 'website',
+      ttl: 60 * 60 * 24 * 7
     },
     twitter: {
       title,
       description,
-      cardType: 'summary_large_image',
-      handle: '@clankfun',
       images: [data.img_url ? data.img_url : 'https://clank.fun/og.png'],
-    }
+    },
   }
 }
 
