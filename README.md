e8ae2b63ab46d495691dfc5d7d47129f237b4ea7
Nick Tikhonov <ndtikhonov@gmail.com>
2024-12-15 09:59:13 +0100

    Clean CA button

--- README.md
+++ README.md
@@ -4,6 +4,7 @@ easily browse and trade clanker memecoins on Base
 
 ## Contribute
 
+* make sure you're on Node `v19.9.0`
 * follow the instructions in `.env.example` file to understand what you'll need to configure
 * copy envs in `.env.example` to `.env`
 * make sure to run `build` before you raise any PRs to make sure the build passes
--- src/app/swap.tsx
+++ src/app/swap.tsx
@@ -19,6 +19,7 @@ import { FFromInput, FToInput } from "./components/FSwapper";
 import { FButton } from "./components/FButton";
 import { Button } from "~/components/ui/button";
 import { categorizeAmt } from '~/lib/analytics';
+import { ExternalLink } from 'lucide-react';
 
 const MAX_ALLOWANCE =
   115792089237316195423570985008687907853269984665640564039457584007913129639935n;
@@ -300,8 +301,9 @@ export function SwapInterface({
         <FButton onClick={() => handleTabSwitch(false)} selected={!isBuying}>
           Sell
         </FButton>
-        <FButton onClick={() => copyCA()}>
-          Copy CA
+        <div className='flex-grow'/>
+        <FButton onClick={() => copyCA()} faded>
+          <ExternalLink size={16} />
         </FButton>
       </div>
       <div className="flex flex-col gap-1">
