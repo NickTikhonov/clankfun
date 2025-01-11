e74d13a2a7b6e1717bc0232f7f2e604dc4596fb9
Nick Tikhonov <ndtikhonov@gmail.com>
2025-01-11 16:58:50 -0500

    Index duration

--- src/app/api/index/route.ts
+++ src/app/api/index/route.ts
@@ -1,3 +1,6 @@
+export const maxDuration = 300; // This function can run for a maximum of 5 seconds
+export const dynamic = 'force-dynamic';
+
 import { NextResponse } from 'next/server';
 import { z } from 'zod';
 import { embueClankers } from '~/app/server';
