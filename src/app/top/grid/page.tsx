6832c3fcea778c6cde77de41d7ae7f8411e2c7d0
Nick Tikhonov <ndtikhonov@gmail.com>
2025-01-23 13:46:38 +0000

    Fix

--- src/app/top/grid/page.tsx
+++ src/app/top/grid/page.tsx
@@ -21,16 +21,14 @@ export default async function Page({
   return (
     <div className="w-full flex flex-wrap items-start justify-start p-4 bg-purple-500">
       {data.filter((c) => c.img_url).map((clanker) => (
-        <img
-          key={clanker.contract_address}
-          src={clanker.img_url ?? ""}
-          alt={clanker.name}
-          className="w-[50px] h-[50px] object-cover"
-          onClick={() => {
-            // launch clank.fun
-            window.open(`https://clank.fun/t/${clanker.contract_address.toLowerCase()}`, '_blank')
-          }}
-        />
+        <a href={`/t/${clanker.contract_address.toLowerCase()}`} key={clanker.contract_address} className="p-0">
+          <img
+            key={clanker.contract_address}
+            src={clanker.img_url ?? ""}
+            alt={clanker.name}
+            className="w-[50px] h-[50px] object-cover"
+          />
+        </a>
       ))}
     </div>
   )
